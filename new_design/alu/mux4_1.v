545a36b36c19279dedd9dfe0e244ba46bc2dc60f
Huoyongtony <46124508+Huoyongtony@users.noreply.github.com>
2021-05-25 20:56:50 +0100

    Update mux4_1.v

--- new_design/alu/mux4_1.v
+++ new_design/alu/mux4_1.v
@@ -8,3 +8,18 @@ output out);
 assign out = s1 ? (s0 ? d : c) : (s0 ? b : a);
 
 endmodule
+
+module m41_32 ( input[31:0] a,  
+input[31:0] b,  
+input[31:0] c,  
+input[31:0] d,  
+input s0, s1, 
+output out);
+
+generate
+	for (i=0: i<32; i = i+1)begin
+		m41 m41_inst (a[i],b[i],c[i],d[i],s0,s1);
+	end
+endgenerate
+
+endmodule

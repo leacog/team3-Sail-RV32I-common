fd80168650f7760212c025257aad32fa9e39be06
Huoyongtony <46124508+Huoyongtony@users.noreply.github.com>
2021-05-25 23:52:06 +0100

    sim alu2

--- simulation/my_alu/alu_v2.v
+++ simulation/my_alu/alu_v2.v
@@ -1,25 +1,20 @@
 /*
 	Authored 2018-2019, Ryan Voo.
-
 	All rights reserved.
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions
 	are met:
-
 	*	Redistributions of source code must retain the above
 		copyright notice, this list of conditions and the following
 		disclaimer.
-
 	*	Redistributions in binary form must reproduce the above
 		copyright notice, this list of conditions and the following
 		disclaimer in the documentation and/or other materials
 		provided with the distribution.
-
 	*	Neither the name of the author nor the names of its
 		contributors may be used to endorse or promote products
 		derived from this software without specific prior written
 		permission.
-
 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -41,14 +36,11 @@
 
 
 
-
-
 /*
  *	Description:
  *
  *		This module implements the ALU for the RV32I.
  */
-
 
 
 
@@ -64,6 +56,9 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 	output reg [31:0]	ALUOut;
 	output reg		Branch_Enable;
 
+	wire [31:0] DSPadd; //Wire to connect to DSP sum
+	wire [31:0] DSPsub; //Wire to connect to DSP subraction result
+
 	/*
 	 *	This uses Yosys's support for nonzero initial values:
 	 *
@@ -78,82 +73,89 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 		Branch_Enable = 1'b0;
 	end
 
-	wire[31:0] mu0_out;
-	wire[31:0] mu1_out;
-	wire[31:0] mu2_out;
-	wire[31:0] mu3_out;
 	
-
-	m41_32 mu0(
-		.a(A ^ B),
-		.b(A),
-		.c(A | B),
-		.d((~A) & B),
-		.s1(ALUctl[1]),
-		.s0(ALUctl[0]),
-		.out(mu0_out)
+	DSPadder alu_adder(
+			.input1(A),
+			.input2(B),
+			.out(DSPadd)
 		);
 
-	m41_32 mu1(
-		.a(A >>> B[4:0]),
-		.b(A << B[4:0]),
-		.c(A - B),
-		.d(($signed(A) < $signed(B) ? 32'b1 : 32'b0)),
-		.s1(ALUctl[1]),
-		.s0(ALUctl[0]),
-		.out(mu1_out)
-	);
+	DSPsubtractor alu_subbtractor(
+			.input1(A),
+			.input2(B),
+			.out(DSPsub)
+		);
 
-	m41_32 mu2(
-		.a(A & B),
-		.b(A | B ),
-		.c(A + B),
-		.d(A >> B[4:0]),
-		.s1(ALUctl[1]),
-		.s0(ALUctl[0]),
-		.out(mu2_out)
-	);
 
-	m41_32 mu3(
-		.a(mu2_out),
-		.b(mu1_out ),
-		.c(32'b00),
-		.d(mu0_out),
-		.s1(ALUctl[3]),
-		.s0(ALUctl[2]),
-		.out(mu3_out)
-	);
 
 	always @(ALUctl, A, B) begin
-		ALUOut <= mu3_out;
+		case (ALUctl[3:0])
+			/*
+			 *	AND (the fields also match ANDI and LUI)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_AND:	ALUOut = A & B;
+
+			/*
+			 *	OR (the fields also match ORI)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_OR:	ALUOut = A | B;
+
+			/*
+			 *	ADD (the fields also match AUIPC, all loads, all stores, and ADDI)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_ADD:	ALUOut = DSPadd;
+
+			/*
+			 *	SUBTRACT (the fields also matches all branches)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SUB:	ALUOut = DSPsub;
+
+			/*
+			 *	SLT (the fields also matches all the other SLT variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SLT:	ALUOut = DSPsub[31] ? 32'b1 : 32'b0;
+
+			/*
+			 *	SRL (the fields also matches the other SRL variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SRL:	ALUOut = A >> B[4:0];
+
+			/*
+			 *	SRA (the fields also matches the other SRA variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SRA:	ALUOut = A >>> B[4:0];
+
+			/*
+			 *	SLL (the fields also match the other SLL variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SLL:	ALUOut = A << B[4:0];
+
+			/*
+			 *	XOR (the fields also match other XOR variants)
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_XOR:	ALUOut = A ^ B;
+
+			/*
+			 *	CSRRW  only
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_CSRRW:	ALUOut = A;
+
+			/*
+			 *	CSRRS only
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_CSRRS:	ALUOut = A | B;
+
+			/*
+			 *	CSRRC only
+			 */
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_CSRRC:	ALUOut = (~A) & B;
+
+			/*
+			 *	Should never happen.
+			 */
+			default:					ALUOut = 0;
+		endcase
 	end
-
-	/*
-	always @(ALUctl, ALUOut, A, B) begin
-		ALUOut = (ALUctl[3])? (
-		(ALUctl[1])? (
-			(ALUctl[0])? (~A) & B : A | B
-		):(
-			(ALUctl[0])? A : A ^ B
-		)
-	):(
-		(ALUctl[2])?(
-			(ALUctl[1])?(
-				(ALUctl[0])? ($signed(A) < $signed(B) ? 32'b1 : 32'b0) : A - B
-			):(
-				(ALUctl[0])? A << B[4:0] : A >>> B[4:0]
-			)
-		):(
-			(ALUctl[1])?(
-				(ALUctl[0])? A >> B[4:0] : A + B
-			):(
-				(ALUctl[0])? A | B : A & B
-			)
-		)
-	);
-	end*/
-
-	
 
 	always @(ALUctl, ALUOut, A, B) begin
 		case (ALUctl[6:4])

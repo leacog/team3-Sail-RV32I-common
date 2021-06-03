b063fe8a8d4063ac35a70648e143c853c23fab43
Huoyongtony <46124508+Huoyongtony@users.noreply.github.com>
2021-06-03 13:07:50 +0100

    Update instruction_mem_v2.v

--- verilog/instruction_mem_v2.v
+++ verilog/instruction_mem_v2.v
@@ -50,7 +50,7 @@ module instruction_memory(addr, out, clk);
 	
 	initial begin
 		$readmemh("verilog/program.hex",insmem);
-		$readmemh("verilog/program.hex",out);
+		out = 8'h00000013;
 	end
 	
 	always @(posedge clk) begin

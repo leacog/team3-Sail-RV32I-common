41357190ad22d011cd812c52c0398564e4c53e4f
leacog <mikael.cognell@gmail.com>
2021-06-08 09:01:19 +0000

    clean

---
+++ subsetting/alu-subset-includes.v
@@ -0,0 +1,14 @@
+`define ALU_AND 1'b1
+`define ALU_OR 1'b1
+`define ALU_ADD 1'b1
+`define ALU_SUB 1'b1
+`define ALU_SLT 1'b1
+`define ALU_SRL 1'b1
+`define ALU_SLL 1'b1
+`define ALU_XOR 1'b1
+`define ALU_BEQ 1'b1
+`define ALU_BNE 1'b1
+`define ALU_BLT 1'b1
+`define ALU_BGE 1'b1
+`define ALU_BLTU 1'b1
+`define ALU_BGEU 1'b1
